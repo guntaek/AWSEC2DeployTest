7ebd8c80c0b42670f17d1dbc02a8d3f246b3c148
guntaek <rjsxor7584@naver.com>
2025-06-27 09:28:47 +0900

    first

---
+++ src/test/java/springbootbasic/awsec2deploytest/Awsec2DeployTestApplicationTests.java
@@ -0,0 +1,13 @@
+package springbootbasic.awsec2deploytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Awsec2DeployTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
