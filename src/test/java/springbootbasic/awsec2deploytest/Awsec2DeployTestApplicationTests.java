0123b44ce463c041986b021394a6f45b078ab855
guntaek <rjsxor7584@naver.com>
2025-06-27 11:20:10 +0900

    deploy 파일 추가

--- src/test/java/springbootbasic/awsec2deploytest/Awsec2DeployTestApplicationTests.java
+++ src/test/java/springbootbasic/awsec2deploytest/Awsec2DeployTestApplicationTests.java
@@ -3,11 +3,16 @@ package springbootbasic.awsec2deploytest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class Awsec2DeployTestApplicationTests {
 
+    String str1 = "Hello";
+
     @Test
-    void contextLoads() {
+    void test() {
+        assertEquals(str1, "Hello");
     }
 
 }
